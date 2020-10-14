c244cb6f9f82fd04394acca1f1fed48a1d412bc2
Martel <martel.mosqueda@alumnos.udg.mx>
2020-10-13 19:40:10 -0500

    Implementada función burbuja

--- burbuja.go
+++ burbuja.go
@@ -1,9 +1,21 @@
 package main
 
-func Burbuja(s []int64)  {
+func Burbuja(s []int64) {
+	// Total elements
+	total := len(s)
+	var temp int64
 
+	for i := 0; i < total; i++ {
+		for j := 0; j < total; j++ {
+			if s[i] < s[j] {
+				temp = s[i]
+				s[i] = s[j]
+				s[j] = temp
+			}
+		}
+	}
 }
 
-func main()  {
-	
+func main() {
+
 }
